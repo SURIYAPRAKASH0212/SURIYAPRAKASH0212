8082014d515d7b80091d32671f76049480a77d88
SURIYA <suriya22714@gmail.com>
2025-08-10 04:24:18 +0000

    Sync LeetCode submission Runtime - 3 ms (3.26%), Memory - 7.9 MB (100.00%)

---
+++ leetcode-solutions/0231-power-of-two/solution.c
@@ -0,0 +1,15 @@
+bool isPowerOfTwo(int n) {
+    int flag=0;
+    for(int i=0;i<=sqrt(n)+1;i++){
+        int a=pow(2,i);
+        if(a==n){
+            flag=1;
+        }
+    }
+    if(flag==1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
