1db4ef30c4d159c5be7839aca0dd9ee8d7371d67
SURIYA <suriya22714@gmail.com>
2025-08-18 05:52:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.2 MB (100.00%)

---
+++ leetcode-solutions/0035-search-insert-position/solution.c
@@ -0,0 +1,11 @@
+int searchInsert(int* nums, int n, int target) {
+    for(int i=0;i<n;i++){
+        if(nums[i]==target){
+            return i;
+        }
+        else if(nums[i]>target){
+            return i;
+        }
+    }
+    return n;
+}
