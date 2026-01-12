f152d5cb111da0f1070d75b70ae5fee927c67d94
SURIYA <suriya22714@gmail.com>
2026-01-12 15:13:33 +0000

    Sync LeetCode submission Runtime - 1 ms (97.88%), Memory - 46.9 MB (56.02%)

---
+++ leetcode-solutions/3846-minimum-operations-to-make-array-sum-divisible-by-k/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int sum=0,count=0;;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        while(sum%k!=0){
+            sum--;
+            count++;
+        }
+        return count;
+    }
+}
