706848fcedecacf78d859261f92a310b780306e8
SURIYA <suriya22714@gmail.com>
2026-02-22 05:02:42 +0000

    Sync LeetCode submission Runtime - 1 ms (26.06%), Memory - 42.7 MB (9.54%)

---
+++ leetcode-solutions/3918-check-divisibility-by-digit-sum-and-product/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkDivisibility(int num) {
+        int original=num;
+        int sum=0,product=1;
+        for(int i=num;num!=0;i++){
+            int a=num%10;
+            sum+=a;
+            product*=a;
+            num/=10;
+        }
+        int b=sum+product;
+        if(original%b==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
