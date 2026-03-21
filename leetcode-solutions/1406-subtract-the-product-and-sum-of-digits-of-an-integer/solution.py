540806a1637808b62e5c2d745cfaf85ee1f9d773
SURIYA <suriya22714@gmail.com>
2026-03-21 05:03:11 +0000

    Sync LeetCode submission Runtime - 3 ms (4.50%), Memory - 12.4 MB (16.81%)

---
+++ leetcode-solutions/1406-subtract-the-product-and-sum-of-digits-of-an-integer/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def subtractProductAndSum(self, n):
+        sum=0
+        pro=1
+        while(n>0):
+            digit=n%10
+            sum+=digit
+            pro*=digit
+            n//=10
+        return pro-sum
