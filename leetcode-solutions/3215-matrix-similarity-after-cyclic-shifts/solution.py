4447d9b563482be29f2e28f467f81f7f05c90381
SURIYA <suriya22714@gmail.com>
2026-03-27 01:43:17 +0000

    Sync LeetCode submission Runtime - 15 ms (20.18%), Memory - 12.2 MB (98.17%)

---
+++ leetcode-solutions/3215-matrix-similarity-after-cyclic-shifts/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def areSimilar(self, mat, k):
+        m,n=len(mat),len(mat[0])
+        k%=n
+        for i in range(m):
+            for j in range(n):
+                if i%2==0:
+                    new_col=(j+k)%n
+                else:
+                    new_col=(j-k)%n
+                if mat[i][j]!=mat[i][new_col]:
+                    return False
+        return True
