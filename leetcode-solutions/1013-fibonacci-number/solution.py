92025fb31eb73df360f547fb999f269cd404290b
SURIYA <suriya22714@gmail.com>
2026-04-01 02:13:35 +0000

    Sync LeetCode submission Runtime - 14 ms (79.71%), Memory - 12.3 MB (87.33%)

---
+++ leetcode-solutions/1013-fibonacci-number/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def fib(self, n):
+        if n<=1:
+            return n
+        prev2,prev1=0,1
+        for _ in range(2,n+1):
+            prev2,prev1=prev1,prev1+prev2
+        return prev1
